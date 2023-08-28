7845491088eccec148f2bdc9d2970740731b7ae9
Lai Jiang <tonylaijiang@gmail.com>
2023-08-28 05:27:58 -0700

    json fixed, but image crop icon sometimes empty

--- actions.py
+++ actions.py
@@ -1,5 +1,12 @@
 import cv2
 from paddleocr import PaddleOCR
+import base64
+
+def convert_image_to_base64(img):
+    _, buffer = cv2.imencode(img)
+    img_str = base64.b64encode(buffer).decode('utf-8')
+    return img_str
+
 
 def get_elements(img):
     """
@@ -15,10 +22,19 @@ def get_elements(img):
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
         icon = img[y:y+h, x:x+w]
-        icons.append((icon, (x, y)))
+        icons.append((convert_image_to_base64(icon), (x, y)))
     
     # word extraction
-    ocr = PaddleOCR.OCR(lang="en") 
-    texts = ocr.ocr(img)
-    text_data = [(line[-1][0], line[-1][-1]) for line in texts] 
-    return {"icons": icons, "text_data": text_data}
+    ocr = PaddleOCR(use_angle_cls=True, lang="en") 
+    texts = ocr.ocr(img, cls=True)
+
+    text_data = []
+    for line in texts[0]:
+        text_coords = line[0]
+        text_x = sum([text_coords[i][0] for i in range(4)]) / 4
+        text_y = sum([text_coords[i][1] for i in range(4)]) / 4
+        text = line[-1][0]
+        text_data.append((text, (text_x, text_y)))
+    
+
+    return {"icons": icons, "text_data": text_data} 
